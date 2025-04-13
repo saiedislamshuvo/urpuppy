b919f35747285ae7bcce0c55da8fe08efcbde981
Andrew Peroramas <wolfandrew307@gmail.com>
2025-04-13 19:20:51 +0800

    add migration to pgsql

--- database/migrations/2025_02_04_101936_change_company_state_id_in_users.php
+++ database/migrations/2025_02_04_101936_change_company_state_id_in_users.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -12,7 +13,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('company_state_id')->nullable()->change();
+                if (DB::getDriverName() === 'sqlite') {
+        $table->unsignedBigInteger('company_state_id')->nullable()->change();
+    } elseif (DB::getDriverName() === 'pgsql') {
+        DB::statement('ALTER TABLE users ALTER COLUMN company_state_id DROP NOT NULL');
+    }
         });
     }
 
