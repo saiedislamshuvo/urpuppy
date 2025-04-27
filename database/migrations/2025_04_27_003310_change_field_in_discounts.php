3e96b3b20d259502e2e0f0468a1227e50c03e9d4
Andrew Peroramas <wolfandrew307@gmail.com>
2025-04-27 08:42:22 +0800

    change migration

---
+++ database/migrations/2025_04_27_003310_change_field_in_discounts.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('discounts', function (Blueprint $table) {
+            $table->text('targeted_emails')->change(); // Latitude: 10 total digits, 8 decimal places
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
