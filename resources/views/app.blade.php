e1e6dbcfaa1b6ed3b7264b69cb16664bc937f8e8
Andrew Peroramas <wolfandrew307@gmail.com>
2025-05-31 11:47:08 +0800

    add schema

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,28 +6,38 @@
     <meta name="author" content="UrPuppy.com">
     <meta name="robots" content="index, follow">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Your go-to platform for adorable puppies and pet-related content.">
 
-    <link fetchpriority="high" rel="preload" href="{{ asset('banner-bg.webp') }}">
-    <link rel="icon" type="image/x-icon" href="../images/logos/favicon.png">
+    <link fetchpriority="high" rel="preload" as="image" href="{{ asset('banner-bg.webp') }}">
+    <link rel="icon" type="image/png" href="{{ asset('images/logos/favicon.png') }}">
 
-    <!-- Scripts -->
     @viteReactRefresh
     @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
     @inertiaHead
+
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2165217194424630"
         crossorigin="anonymous"></script>
 
-
     <script defer src="https://analytics.761073128.xyz/script.js" data-website-id="ff04b39a-2c33-43df-9854-8edb4ae01c4a">
     </script>
-    <script type="application/ld+json">{"@context":"https:\/\/schema.org","@type":"LocalBusiness","name":"Urpuppy.com","email":"support@urpuppy.com","contactPoint":{"@type":"ContactPoint","areaServed":"US"},"image":"https:\/\/urpuppy.com\/logo.png"}</script>
 
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org",
+            "@type": "LocalBusiness",
+            "name": "Urpuppy.com",
+            "email": "support@urpuppy.com",
+            "contactPoint": {
+                "@type": "ContactPoint",
+                "areaServed": "US"
+            },
+            "image": "https://urpuppy.com/logo.png"
+        }
+    </script>
 </head>
 
 <body class="font-sans antialiased">
     @inertia
-
-
 
     <noscript>
         <style>
@@ -37,8 +47,7 @@
             }
         </style>
     </noscript>
-
-
 </body>
 
 </html>
+
