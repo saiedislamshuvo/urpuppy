bd002dcaf323f3ce7ebdaab817daf0dee399037a
Andrew Peroramas <wolfandrew307@gmail.com>
2025-06-01 15:02:57 +0800

    fix csp

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -17,7 +17,14 @@ class ContentSecurityPolicy
     {
         $response = $next($request);
 
-                $csp = "default-src 'self'; script-src 'self' https://pagead2.googlesyndication.com https://analytics.761073128.xyz https://ep2.adtrafficquality.google https://maps.googleapis.com 'unsafe-inline'; script-src-elem 'self' https://pagead2.googlesyndication.com https://analytics.761073128.xyz https://ep2.adtrafficquality.google https://maps.googleapis.com 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' https: data: https://maps.googleapis.com; font-src 'self' data:; frame-src https://googleads.g.doubleclick.net https://tpc.googlesyndication.com; connect-src 'self' https://analytics.761073128.xyz https://ep1.adtrafficquality.google https://maps.googleapis.com;";
+        $csp = "default-src 'self';
+                script-src 'self' https://pagead2.googlesyndication.com https://analytics.761073128.xyz https://ep2.adtrafficquality.google https://maps.googleapis.com https://js.stripe.com 'unsafe-inline';
+                script-src-elem 'self' https://pagead2.googlesyndication.com https://analytics.761073128.xyz https://ep2.adtrafficquality.google https://maps.googleapis.com https://js.stripe.com 'unsafe-inline';
+                style-src 'self' 'unsafe-inline';
+                img-src 'self' https: data: https://maps.googleapis.com;
+                font-src 'self' data:;
+                frame-src https://googleads.g.doubleclick.net https://tpc.googlesyndication.com https://ep2.adtrafficquality.google https://www.google.com/recaptcha;
+                connect-src 'self' https://analytics.761073128.xyz https://ep1.adtrafficquality.google https://maps.googleapis.com https://js.stripe.com;";
 
         $response->headers->set('Content-Security-Policy', $csp);
 
