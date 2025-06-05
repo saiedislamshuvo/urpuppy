6713aa6de09c66ab2bd2fe052e221d4af17ab83c
Andrew <wolfandrew307@gmail.com>
2025-06-05 12:15:41 +0800

    fix address in card

--- resources/js/Components/Puppy/Card.tsx
+++ resources/js/Components/Puppy/Card.tsx
@@ -49,7 +49,7 @@ const PuppyCard = ({puppy, className="col-md-6 col-lg-4 col-xl-3 mb-4", height="
                     <div className="col-6">
                       <div className="d-flex align-items-center gap-2 mb-2">
                         <img loading="lazy" src="/images/svgs/icon-map-pin.svg" alt="urpuppy-img" width="20" height="20" />
-                        <p className="mb-0 ">{puppy.seller?.short_address}</p>
+                        <p className="mb-0 truncate">{puppy.seller?.address}</p>
                       </div>
                     </div>
                     <div className="col-6">
